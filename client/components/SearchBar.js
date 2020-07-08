9450705bb0983533c32cfaf69cc7c8df32ffb88c
kajoban <kuhapark@mcmaster.ca>
2020-07-08 16:51:07 -0400

    added title and searchbar

---
+++ client/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { StyleSheet, View, TextInput } from "react-native";
+
+export default function UselessTextInput() {
+  const [value, onChangeText] = useState();
+  console.log(value);
+  return (
+    <View>
+      <TextInput
+        style={styles.searchBar}
+        onChangeText={(text) => onChangeText(text)}
+        value={value}
+        placeholder="Search"
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  searchBar: {
+    fontFamily: "Roboto_400Regular",
+    fontSize: 20,
+    textAlign: "left",
+    height: 50,
+    borderWidth: 2,
+    borderColor: "#C4C4C4",
+    borderRadius: 15,
+    backgroundColor: "#EEEEEE",
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+});
---
+++ client/components/Title.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View contentContainerStyle={{ alignItems: "left" }}>
+      <Text style={styles.title}>Recollection</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "Roboto_700Bold",
+    fontSize: 35,
+    marginVertical: 20,
+    marginHorizontal: 10,
+  },
+});
