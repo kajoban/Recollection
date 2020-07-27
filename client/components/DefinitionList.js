af99a6d61fa8f9d09a95d2c2d1a17baedaf11e68
kajoban <kuhapark@mcmaster.ca>
2020-07-27 17:34:32 -0400

    fixed key issue

--- client/components/DefinitionList.js
+++ client/components/DefinitionList.js
@@ -2,7 +2,7 @@ import React from "react";
 import DefinitionCard from "./DefinitionCard";
 
 export default function DefinitionList({ definitions }) {
-  return definitions.map((definition) => {
-    return <DefinitionCard definition={definition} />;
+  return definitions.map((definition, i) => {
+    return <DefinitionCard key={i} definition={definition} />;
   });
 }
