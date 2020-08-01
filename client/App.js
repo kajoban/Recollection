a3d75a0ebf3a1631ab1b6c62127054580f3267b2
kajoban <kuhapark@mcmaster.ca>
2020-07-31 23:47:50 -0400

    fixed scroll view, added async storage

--- client/App.js
+++ client/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, ScrollView, Text } from "react-native";
+import { StyleSheet, View, ScrollView, Text } from "react-native";
 import {
   useFonts,
   Roboto_700Bold,
   Roboto_400Regular,
   Roboto_500Medium,
 } from "@expo-google-fonts/dev";
+import AsyncStorage from "@react-native-community/async-storage";
 import Title from "./components/Title";
 import Subtitle from "./components/Subtitle";
 import SearchBar from "./components/SearchBar";
@@ -23,6 +24,29 @@ export default function App() {
   const [definitionData, setDefinitionData] = useState({});
   console.log("query is: ", query);
 
+  // const storeDefinition = async (value) => {
+  //   console.log('storing def')
+  //   try {
+  //     await AsyncStorage.getItem('savedDefinitions', (err, result) => {
+  //       console.log('getting definitions')
+  //       var newDefiniton = {word: value.word, definition: value.definitions[0].definition}
+  //       if (result !== null) {
+  //         console.log('adding to list of definitions')
+  //         var newDefinitons = JSON.parse(result).concat(newDefiniton);
+  //         await AsyncStorage.setItem('savedDefinitions', JSON.stringify(newDefinitons));
+  //         console.log(newDefinitons)
+  //       } else {
+  //         console.log('creating new list of definitons')
+  //         await AsyncStorage.setItem('savedDefinitions', JSON.stringify([newDefiniton]));
+  //         console.log(newDefiniton)
+  //       }
+  //     });
+  //   } catch (e) {
+  //     console.log("error storing definition")
+  //     console.log(e)
+  //   }
+  // }
+
   useEffect(() => {
     if (query) {
       fetch(`https://owlbot.info/api/v4/dictionary/${query}`, {
@@ -35,6 +59,7 @@ export default function App() {
         .then((json) => {
           console.log(json);
           setDefinitionData(json);
+          //storeDefinition(json);
         })
         .catch((error) => console.log(error));
     }
@@ -49,22 +74,25 @@ export default function App() {
     );
   } else {
     return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <Title title={"Recollection"} />
-        <SearchBar setQuery={setQuery} />
-        <Definition definitionData={definitionData} />
-        <Subtitle subtitle={"Recently Searched"} />
-        <StatusBar style="auto" />
-      </ScrollView>
+      <View style={styles.OuterContainer}>
+        <ScrollView>
+          <Title title={"Recollection"} />
+          <SearchBar setQuery={setQuery} />
+          <Definition definitionData={definitionData} />
+          <Subtitle subtitle={"Recently Searched"} />
+          <StatusBar style="auto" />
+        </ScrollView>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  OuterContainer: {
     flex: 1,
+    flexDirection: "column",
     backgroundColor: "#fff",
-    marginVertical: 50,
     marginHorizontal: 20,
+    marginVertical: 20,
   },
 });
